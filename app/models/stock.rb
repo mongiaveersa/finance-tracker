b0feaab9c07c83b9e787e164125cb7bd4f06ae28
Shubham Mongia <shubham@veersatech.com>
2021-09-02 20:44:20 +0530

    Added API and data shown

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -6,8 +6,13 @@ class Stock < ApplicationRecord
             secret_token: 'Tsk_036c3b9633c542988502dea5f9e31a22',
             endpoint: 'https://sandbox.iexapis.com/v1'
           )
-
-        return client.price(ticker_symbol)
+          begin
+            s= new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol) )
+          rescue => exception
+              return nil
+          end
+          
+          return s
     end
 
 
